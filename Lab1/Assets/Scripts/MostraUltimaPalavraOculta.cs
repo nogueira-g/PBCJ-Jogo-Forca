d6651daed5250d7fd3b2a47387b515b5dfe1bb20
aleexalvz <alex.alves_br@hotmail.com>
2022-03-05 15:26:07 -0300

    atualiza scripts de credito

--- Lab1/Assets/Scripts/MostraUltimaPalavraOculta.cs
+++ Lab1/Assets/Scripts/MostraUltimaPalavraOculta.cs
@@ -8,7 +8,6 @@ public class MostraUltimaPalavraOculta : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Text>().text = PlayerPrefs.GetString("palavraOculta");
+        GetComponent<Text>().text = PlayerPrefs.GetString("palavraOculta"); //coloca a ultima palavra oculta no componente
     }
-
 }
--- Lab1/Assets/Scripts/TelaCreditos.cs
+++ Lab1/Assets/Scripts/TelaCreditos.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using UnityEngine.SceneManagement;
 
 public class TelaCreditos : MonoBehaviour
 {
 
-    int tempo = 0;
+    float speed = 0.1f;
+    int time = 0;
+
     // Start is called before the first frame update
-    public float speed;
     void Update()
     {
         LoadCreditos();
         VerificaTempoDeCreditos();
+        time++;
     }
+
+    //Faz o movimento vertical dos créditos de acordo com o tempo
     void LoadCreditos()
     {
         Vector3 posicao;
-        posicao = new Vector3(transform.position.x , transform.position.y + speed, transform.position.z) ;
+        posicao = new Vector3(transform.position.x , transform.position.y + speed, transform.position.z);
         transform.position = posicao;
     }
 
+    //Define o tempo de exibição dos créditos, para retornar ao inicio
     void VerificaTempoDeCreditos()
     {
-        if(tempo>=6700)
+        if(time>=12000)
         {
             SceneManager.LoadScene("Lab1_start");
         }
-        tempo++;
     }
 }
